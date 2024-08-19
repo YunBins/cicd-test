c4c1aea829bd7df32e72748808295b8a109e46a5
YunBins <kig0632@naver.com>
2024-08-19 15:34:39 +0900

    fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,25 +5,25 @@ cd /home/ec2-user/app
 DOCKER_APP_NAME=spring
 
 # 실행중인 blue가 있는지
-EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 # green이 실행중이면 blue up
 if [ -z "$EXIST_BLUE" ]; then
-	echo "blue up"
-	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+        echo "blue up"
+        sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
 
-	sleep 30
+        sleep 30
 
-	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-	sudo docker image prune -af # 사용하지 않는 이미지 삭제
+        sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+        sudo docker image prune -af # 사용하지 않는 이미지 삭제
 
 # blue가 실행중이면 green up
 else
-	echo "green up"
-	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+        echo "green up"
+        sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
-	sleep 30
+        sleep 30
 
-	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
-	sudo docker image prune -af
+        sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+        sudo docker image prune -af
 fi
